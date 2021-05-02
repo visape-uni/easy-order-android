782ef0ca4a9e908c97f4aaa83ee926c5e077a23d
visape <vikktor.1996@hotmail.com>
2021-05-02 13:03:26 +0200

    Guardar QR

--- app/src/main/java/edu/uoc/easyorderfront/ui/order/GetQrCodeActivity.kt
+++ app/src/main/java/edu/uoc/easyorderfront/ui/order/GetQrCodeActivity.kt
@@ -1,10 +1,14 @@
 package edu.uoc.easyorderfront.ui.order
 
+import android.content.ContentValues
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import edu.uoc.easyorderfront.R
 import kotlinx.android.synthetic.main.bottom_fragment_get_qr_code.view.*
@@ -30,5 +34,36 @@ class GetQrCodeActivity(
 
         view.img_code_qr.setImageBitmap(bitmap)
 
+        view.btn_guardar.setOnClickListener({
+            guardarFichero()
+        })
+
+    }
+
+    fun guardarFichero() {
+
+        val fileName = "QR_code_${System.currentTimeMillis()}.jpg"
+
+        val values = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            put(MediaStore.Images.Media.IS_PENDING, 1)
+        }
+
+        val resolver = context?.contentResolver
+
+        val collection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        val itemUri = resolver?.insert(collection, values)
+        if (itemUri != null) {
+            resolver.openFileDescriptor(itemUri, "w").use { parcelFileDescriptor ->
+                val acos = ParcelFileDescriptor.AutoCloseOutputStream(parcelFileDescriptor)
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, acos)
+            }
+            values.clear()
+            values.put(MediaStore.Images.Media.IS_PENDING, 0)
+            resolver.update(itemUri, values, null, null)
+            Toast.makeText(context, "El QR se ha guardado correctamente", Toast.LENGTH_LONG).show()
+        }
     }
 }
